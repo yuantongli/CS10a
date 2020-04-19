0c73be163bfd2d3e2b1348a51e99f4a95c75f5f5
503494689 <62361035+503494689@users.noreply.github.com>
2020-04-18 20:53:25 -0400

    Update  Area of ​​Triangle.py

--- Final Project/ Area of ​​Triangle.py
+++ Final Project/ Area of ​​Triangle.py
@@ -1,2 +1,12 @@
 def triangel_area():
-    a = d+ sdd d d
+    one = float(input('Length of the first side: '))
+    two = float(input('Length of the second side: '))
+    three = float(input('Length of the thrid side: '))
+    
+    p = ( one + two + three)/2
+
+    a = (p*(p-one)*(p-two)*(p-three))**(1/2)
+
+    print(a)
+
+triangel_area()
