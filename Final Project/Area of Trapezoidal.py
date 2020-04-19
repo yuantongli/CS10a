92517a5bf9bf1dc438d931e3f60e080e31a680de
503494689 <62361035+503494689@users.noreply.github.com>
2020-04-18 21:37:55 -0400

    Update Area of Trapezoidal.py

--- Final Project/Area of Trapezoidal.py
+++ Final Project/Area of Trapezoidal.py
@@ -1,1 +1,11 @@
-def
+def trapezoidal_are():
+    a = float(input('Upper Sole: '))
+    b = float(input('Bottom Sole: '))
+    c = float(input('Height: '))
+
+    s = (a+b)*c/2
+
+    return s
+
+print(trapezoidal_are())
+
