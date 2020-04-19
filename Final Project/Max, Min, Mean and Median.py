44da74be970531d9c33829a5fd448db2b64cff57
503494689 <62361035+503494689@users.noreply.github.com>
2020-04-18 22:17:56 -0400

    Update Max, Min, Mean and Median.py

--- Final Project/Max, Min, Mean and Median.py
+++ Final Project/Max, Min, Mean and Median.py
@@ -1,1 +1,30 @@
-def
+def max_min_mean_median(list_numbers):
+
+    minN = min(list_numbers)
+
+    maxN = max(list_numbers)
+
+    l = len(list_numbers)
+    list_numbers.sort()
+    if l % 2 == 0:
+        medianone = list_numbers[l//2]
+        mediantwo = list_numbers[l//2 - 1]
+        median = round((medianone+mediantwo)/2)
+    else:
+        median = list_numbers[l//2]
+
+    mean = sum(list_numbers)/len(list_numbers)
+
+    list = []
+
+    list.append(minN)
+    list.append(maxN)
+    list.append(median)
+    list.append(mean)
+
+    return list
+
+    
+    
+
+print(max_min_mean_median([2,3,7,10]))
